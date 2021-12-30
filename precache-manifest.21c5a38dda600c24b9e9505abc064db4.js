363bc28676ab47affc1fc44cfad592ef968e02d4
miklud <miklud.g@gmail.com>
2021-12-30 23:42:33 +0300

    Add files via upload

---
+++ precache-manifest.21c5a38dda600c24b9e9505abc064db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b488107604f70047c3febea0848c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6618d9f38283267089",
+    "url": "/static/css/main.52b06a7b.chunk.css"
+  },
+  {
+    "revision": "022945d9b2c0c50813ea",
+    "url": "/static/js/2.2a5a8f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a5a8f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6618d9f38283267089",
+    "url": "/static/js/main.94378a5f.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  }
+]);
